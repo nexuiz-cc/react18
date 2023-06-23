5d8ec0077212992140b1c2163fdec9febe618dab
nexuiz-cc <41991563+nexuiz-cc@users.noreply.github.com>
2023-06-23 23:17:40 +0900

    ..

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Button } from "antd-mobile";   
+
 const App = ()=>{
     return (
         <>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
 import App from "./App";
 import AntdMobileConfig from "./AntdMobileConfig";
-
 let root = createRoot(document.getElementById("root"));
 
 root.render(
-  <AntdMobileConfig>
-    <App />
-  </AntdMobileConfig>
+  <React.StrictMode>
+    <AntdMobileConfig>
+      <App />
+    </AntdMobileConfig>
+  </React.StrictMode>
 );
